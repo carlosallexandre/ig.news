088952e80c4de1ef1344c8a42ae0b2f5af008492
carlosallexandre <carlos_costarodrigues@outlook.com>
2021-08-24 11:58:58 -0300

    feat: add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <h1 className={styles.title}>
+      Hello world
+    </h1>
   )
 }
