a2889befb66d9f2314d92bd40e6c253a69d125ec
carlosallexandre <carlos_costarodrigues@outlook.com>
2021-08-24 15:04:05 -0300

    chore: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello world
-    </h1>
+    <h1>Hello world</h1>
   )
 }
